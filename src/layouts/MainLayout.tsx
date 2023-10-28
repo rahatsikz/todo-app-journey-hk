61918fb870a3da476f9967a927ebb89482991069
rahatsikz <rascal6271@gmail.com>
2023-10-28 19:03:59 +0600

    [done] - route and Layout setup

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,8 +1,14 @@
 import { Outlet } from "react-router-dom";
+import bgImage from "../assets/bg.png";
 
 const MainLayout = () => {
   return (
-    <div>
+    <div
+      className='min-h-screen bg-repeat-round'
+      style={{
+        backgroundImage: `url(${bgImage})`,
+      }}
+    >
       <Outlet></Outlet>
     </div>
   );
